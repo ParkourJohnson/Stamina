3e494858d100ec29ecacb1d068c3ded7640c4b45
Nosoka <petrovich-bomzh45@bk.ru>
2023-12-21 15:48:52 +0300

    That's something

--- Project1/MyForm.cpp
+++ Project1/MyForm.cpp
@@ -1,68 +1,73 @@
 #include "MyForm.h"
 #include <random>
+#include <cstring>
 
 using namespace System;
 using namespace System::Windows::Forms;
+using namespace System::Runtime::InteropServices;
 
 int correct = 0;
-int mistakes = 0;
 bool isRunning = false;
+char text[] = "Когда человек сознательно или интуитивно выбирает себе в жизни какую-то цель, жизненную задачу, он невольно дает себе оценку. По тому, ради чего человек живет, можно судить и о его самооценке - низкой или высокой. Если человек живет, чтобы приносить людям добро, облегчать их страдания, давать людям радость, то он оценивает себя на уровне этой своей человечности.Он ставит себе цель, достойную человека. Только такая цель позволяет человеку прожить свою жизнь с достоинством и получить настоящую радость.Да, радость!Подумайте: если человек ставит себе задачей увеличивать в жизни добро, приносить людям счастье, какие неудачи могут его постигнуть? Не тому помочь? Но много ли людей не нуждаются в помощи ? Если жить только для себя, своими мелкими заботами о собственном благополучии, то от прожитого не останется и следа.Если же жить для других, то другие сберегут то, чему служил, чему отдавал силы. Можно по - разному определять цель своего существования, но цель должна быть.Надо иметь и принципы в жизни.Одно правило в жизни должно быть у каждого человека, в его цели жизни, в его принципах жизни, в его поведении : надо прожить жизнь с достоинством, чтобы не стыдно было вспоминать. Достоинство требует доброты, великодушия, умения не быть эгоистом, быть правдивым, хорошим другом, находить радость в помощи другим. Ради достоинства жизни надо уметь отказываться от мелких удовольствий и немалых тоже… Уметь извиняться, признавать перед другими ошибку - лучше, чем врать. Обманывая, человек прежде всего обманывает самого себя, ибо он думает, что успешно соврал, а люди поняли и из деликатности промолчали. Жизнь - прежде всего творчество, но это не значит, что каждый человек, чтобы жить, должен родиться художником, балериной или ученым.Можно творить просто добрую атмосферу вокруг себя.Человек может принести с собой атмосферу подозрительности, какого - то тягостного молчания, а может внести сразу радость, свет.Вот это и есть творчество.";
 
-int random(int a, int b)
+int getRandomNumber(int min, int max)
 {
-	srand(time(NULL));
-	if (a > 0) return a + rand() % (b - a);
-	else return a + rand() % (abs(a) + b);
-}
-
-Void Project1::MyForm::timer_Tick(System::Object^ sender, System::EventArgs^ e) {
-	this->label_timeNum->Text = Convert::ToString(Convert::ToDouble(this->label_timeNum->Text) - 0.1);
-	if (Convert::ToDouble(this->label_timeNum->Text) == 0)
-	{
-		this->label_entered_chars->Text = Convert::ToString(correct);
-		this->label_mistakes->Text = Convert::ToString(mistakes);
-		this->label_cpm->Text = Convert::ToString(static_cast<double>(correct)/60.0);
-
-		this->timer->Enabled = false;
-		this->label_cpm->Visible = true;
-		this->label_entered_chars->Visible = true;
-		this->label_mistakes->Visible = true;
-		isRunning = false;
-	}
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    return min + (std::rand() % (max - min + 1));
 }
 
 Void Project1::MyForm::MyForm_Load(System::Object^ sender, System::EventArgs^ e)
 {
-	int randomChar = random(97,122);
-	this->sentence->Text = Convert::ToString(Convert::ToChar(randomChar));
+    this->sentence->Text = Marshal::PtrToStringAnsi((IntPtr)text);
+    this->button_start->Focus();
 }
 
-Void Project1::MyForm::MyForm_KeyDown(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e)
+Void Project1::MyForm::button_start_Click(System::Object^ sender, System::EventArgs^ e)
 {
-	if (Convert::ToString(e->KeyCode)->ToLower() == "return")
-	{
-		isRunning = true;
-		this->timer->Enabled = true;
-	}
+    isRunning = true;
+    this->timer->Enabled = true;
+    this->button_start->Enabled = false;
+}
 
-	if (isRunning == true)
-	{
-		if (Convert::ToString(e->KeyCode)->ToLower() == this->sentence->Text)
-		{
-			int randomChar = random(97, 122);
-			this->sentence->Text = Convert::ToString(Convert::ToChar(randomChar));
-			correct++;
-		}
-		else { mistakes++; }
-	}
-	
+Void Project1::MyForm::timer_Tick(System::Object^ sender, System::EventArgs^ e)
+{
+    this->label_time->Text = Convert::ToString(Convert::ToDouble(this->label_time->Text) - 0.1);
+    if (this->label_time->Text == "0")
+    {
+        isRunning = false;
+        this->timer->Enabled = false;
+        this->label_time->Text = "60";
+        this->button_start->Enabled = true;
+        this->label_correct->Text = Convert::ToString(correct);
+        this->label_cps->Text = Convert::ToString(static_cast<double>(correct) / 60.0);
+        correct = 0;
+    }
+}
+
+Void Project1::MyForm::input_TextChanged(System::Object^ sender, System::EventArgs^ e)
+{
+    if (isRunning == true)
+    {
+        if (this->input->Text == Convert::ToString(this->sentence->Text[0]))
+        {
+            correct++;
+            this->sentence_update();
+        }
+    }
+    this->input->Text = "";
+}
+
+Void Project1::MyForm::sentence_update()
+{
+    memmove(text, text + 1, strlen(text));
+    this->sentence->Text = Marshal::PtrToStringAnsi((IntPtr)text);
 }
 
 [STAThreadAttribute]
 int main(array<String^>^ args)
 {
-	Application::SetCompatibleTextRenderingDefault(false);
-	Application::EnableVisualStyles();
-	Project1::MyForm form;
-	Application::Run(% form);
+    Application::SetCompatibleTextRenderingDefault(false);
+    Application::EnableVisualStyles();
+    Project1::MyForm form;
+    Application::Run(% form);
 }
--- Project1/MyForm.h
+++ Project1/MyForm.h
@@ -40,17 +40,25 @@ namespace Project1 {
 
 
 
-	private: System::Windows::Forms::Label^ label_time;
-	private: System::Windows::Forms::Label^ label_timeNum;
+
+
 
 
 
 
 	private: System::Windows::Forms::Timer^ timer;
-	private: System::Windows::Forms::Label^ label_entered_chars;
-	private: System::Windows::Forms::Label^ label_mistakes;
+	private: System::Windows::Forms::Button^ button_start;
+	private: System::Windows::Forms::Label^ label_correct;
+	private: System::Windows::Forms::Label^ label_cps;
 
-	private: System::Windows::Forms::Label^ label_cpm;
+	private: System::Windows::Forms::Label^ label_time;
+	private: System::Windows::Forms::TextBox^ input;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+
+
+
+
 
 
 
@@ -86,119 +94,144 @@ namespace Project1 {
 		{
 			this->components = (gcnew System::ComponentModel::Container());
 			this->sentence = (gcnew System::Windows::Forms::Label());
-			this->label_time = (gcnew System::Windows::Forms::Label());
-			this->label_timeNum = (gcnew System::Windows::Forms::Label());
 			this->timer = (gcnew System::Windows::Forms::Timer(this->components));
-			this->label_entered_chars = (gcnew System::Windows::Forms::Label());
-			this->label_mistakes = (gcnew System::Windows::Forms::Label());
-			this->label_cpm = (gcnew System::Windows::Forms::Label());
+			this->button_start = (gcnew System::Windows::Forms::Button());
+			this->label_correct = (gcnew System::Windows::Forms::Label());
+			this->label_cps = (gcnew System::Windows::Forms::Label());
+			this->label_time = (gcnew System::Windows::Forms::Label());
+			this->input = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// sentence
 			// 
+			this->sentence->BackColor = System::Drawing::Color::Gold;
+			this->sentence->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
 			this->sentence->Font = (gcnew System::Drawing::Font(L"Times New Roman", 72, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->sentence->Location = System::Drawing::Point(12, 57);
+			this->sentence->Location = System::Drawing::Point(12, 9);
 			this->sentence->Name = L"sentence";
 			this->sentence->Size = System::Drawing::Size(1117, 129);
 			this->sentence->TabIndex = 0;
 			this->sentence->Text = L"Sentence";
-			this->sentence->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
-			// 
-			// label_time
-			// 
-			this->label_time->BackColor = System::Drawing::SystemColors::ButtonFace;
-			this->label_time->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->label_time->Location = System::Drawing::Point(507, 591);
-			this->label_time->Name = L"label_time";
-			this->label_time->Size = System::Drawing::Size(73, 64);
-			this->label_time->TabIndex = 3;
-			this->label_time->Text = L"Время:";
-			this->label_time->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
-			// 
-			// label_timeNum
-			// 
-			this->label_timeNum->BackColor = System::Drawing::SystemColors::ButtonFace;
-			this->label_timeNum->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->label_timeNum->Location = System::Drawing::Point(576, 591);
-			this->label_timeNum->Name = L"label_timeNum";
-			this->label_timeNum->Size = System::Drawing::Size(52, 64);
-			this->label_timeNum->TabIndex = 4;
-			this->label_timeNum->Text = L"60";
-			this->label_timeNum->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->sentence->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
 			// 
 			// timer
 			// 
 			this->timer->Tick += gcnew System::EventHandler(this, &MyForm::timer_Tick);
 			// 
-			// label_entered_chars
+			// button_start
 			// 
-			this->label_entered_chars->BackColor = System::Drawing::SystemColors::ButtonFace;
-			this->label_entered_chars->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->button_start->Font = (gcnew System::Drawing::Font(L"Times New Roman", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label_entered_chars->Location = System::Drawing::Point(493, 263);
-			this->label_entered_chars->Name = L"label_entered_chars";
-			this->label_entered_chars->Size = System::Drawing::Size(148, 23);
-			this->label_entered_chars->TabIndex = 5;
-			this->label_entered_chars->Text = L"Символов введено:";
-			this->label_entered_chars->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
-			this->label_entered_chars->Visible = false;
+			this->button_start->Location = System::Drawing::Point(523, 602);
+			this->button_start->Name = L"button_start";
+			this->button_start->Size = System::Drawing::Size(100, 50);
+			this->button_start->TabIndex = 1;
+			this->button_start->Text = L"START";
+			this->button_start->UseVisualStyleBackColor = true;
+			this->button_start->Click += gcnew System::EventHandler(this, &MyForm::button_start_Click);
 			// 
-			// label_mistakes
+			// label_correct
 			// 
-			this->label_mistakes->BackColor = System::Drawing::SystemColors::ButtonFace;
-			this->label_mistakes->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->label_correct->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->label_correct->Font = (gcnew System::Drawing::Font(L"Times New Roman", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label_mistakes->Location = System::Drawing::Point(493, 286);
-			this->label_mistakes->Name = L"label_mistakes";
-			this->label_mistakes->Size = System::Drawing::Size(148, 23);
-			this->label_mistakes->TabIndex = 6;
-			this->label_mistakes->Text = L"Кол-во ошибок:";
-			this->label_mistakes->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
-			this->label_mistakes->Visible = false;
+			this->label_correct->Location = System::Drawing::Point(618, 288);
+			this->label_correct->Name = L"label_correct";
+			this->label_correct->Size = System::Drawing::Size(70, 25);
+			this->label_correct->TabIndex = 2;
+			this->label_correct->Text = L"-";
+			this->label_correct->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			// 
-			// label_cpm
+			// label_cps
 			// 
-			this->label_cpm->BackColor = System::Drawing::SystemColors::ButtonFace;
-			this->label_cpm->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->label_cps->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->label_cps->Font = (gcnew System::Drawing::Font(L"Times New Roman", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label_cpm->Location = System::Drawing::Point(493, 309);
-			this->label_cpm->Name = L"label_cpm";
-			this->label_cpm->Size = System::Drawing::Size(148, 23);
-			this->label_cpm->TabIndex = 7;
-			this->label_cpm->Text = L"Символов в минуту:";
-			this->label_cpm->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
-			this->label_cpm->Visible = false;
+			this->label_cps->Location = System::Drawing::Point(618, 313);
+			this->label_cps->Name = L"label_cps";
+			this->label_cps->Size = System::Drawing::Size(70, 25);
+			this->label_cps->TabIndex = 3;
+			this->label_cps->Text = L"-";
+			this->label_cps->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// label_time
+			// 
+			this->label_time->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->label_time->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->label_time->Font = (gcnew System::Drawing::Font(L"Times New Roman", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label_time->Location = System::Drawing::Point(1079, 602);
+			this->label_time->Name = L"label_time";
+			this->label_time->Size = System::Drawing::Size(50, 50);
+			this->label_time->TabIndex = 5;
+			this->label_time->Text = L"60";
+			this->label_time->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// input
+			// 
+			this->input->BackColor = System::Drawing::Color::DodgerBlue;
+			this->input->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->input->Location = System::Drawing::Point(31, 632);
+			this->input->Name = L"input";
+			this->input->Size = System::Drawing::Size(100, 13);
+			this->input->TabIndex = 6;
+			this->input->TextChanged += gcnew System::EventHandler(this, &MyForm::input_TextChanged);
+			// 
+			// label1
+			// 
+			this->label1->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(438, 288);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(185, 25);
+			this->label1->TabIndex = 7;
+			this->label1->Text = L"Символов введено:";
+			// 
+			// label2
+			// 
+			this->label2->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Times New Roman", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(438, 313);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(185, 25);
+			this->label2->TabIndex = 8;
+			this->label2->Text = L"Символов в секунду:";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->BackColor = System::Drawing::SystemColors::MenuHighlight;
+			this->BackColor = System::Drawing::Color::DodgerBlue;
 			this->ClientSize = System::Drawing::Size(1141, 664);
-			this->Controls->Add(this->label_cpm);
-			this->Controls->Add(this->label_mistakes);
-			this->Controls->Add(this->label_entered_chars);
-			this->Controls->Add(this->label_timeNum);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->input);
 			this->Controls->Add(this->label_time);
+			this->Controls->Add(this->label_cps);
+			this->Controls->Add(this->label_correct);
+			this->Controls->Add(this->button_start);
 			this->Controls->Add(this->sentence);
 			this->Name = L"MyForm";
 			this->Text = L"Stamina";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
-			this->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &MyForm::MyForm_KeyDown);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	
-	
-	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e);
+	private: System::Void button_start_Click(System::Object^ sender, System::EventArgs^ e);
 
 	private: System::Void timer_Tick(System::Object^ sender, System::EventArgs^ e);
 
-	private: System::Void MyForm_KeyDown(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e);
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e);
 
+	private: System::Void input_TextChanged(System::Object^ sender, System::EventArgs^ e);
+
+	private: System::Void sentence_update();
 };
 }
